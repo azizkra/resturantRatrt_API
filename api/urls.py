c44ee3dfaadbed667365648c5ec6519e4d2a1845
aziz krayyem <azizkrayyem7@gmail.com>
2022-04-06 21:52:52 +0300

    2_Basic_Setup

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.conf.urls import include
+from .views import MealViewSet, RatingViewSet
+
+router = DefaultRouter()
+router.register('meals', MealViewSet)
+router.register('rating', RatingViewSet)
+
+urlpatterns = [
+path('', include(router.urls)),    
+]
+
